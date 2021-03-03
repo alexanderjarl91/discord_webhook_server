fdeba934a71041b59e4b3eb954600dc0f2e90527
Alexander <alexanderjarl91@gmail.com>
2021-03-03 16:44:08 +0000

    server ready

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bp = require("body-parser");
+const fetch = require("node-fetch");
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  fetch(
+    "https://discord.com/api/webhooks/816409393943805963/zcMXnR-RMR8-NTauCoWtwwAEHMau9fjzxXz-9qia4LfYRl6zhUzJO5UPA79vHbLSXJUY",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: "Jarl",
+        avatar:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Chauncey_Billups.jpg/175px-Chauncey_Billups.jpg",
+        content: "testing this webhook",
+      }),
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
